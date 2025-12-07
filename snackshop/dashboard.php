09e25144e884092a48dbd9ca915ce2c061485c79
nurulabsarina <nurulabsarina.a@gmail.com>
2025-12-07 09:12:11 +0800

    Update dashboard.php

--- snackshop/dashboard.php
+++ snackshop/dashboard.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
-    header("Location: index.php");
+    header("Location: login.php");
     exit;
 }
 require 'conn/db.php';
@@ -132,3 +132,4 @@ foreach($top_products as $p) {
 </body>
 
 </html>
+

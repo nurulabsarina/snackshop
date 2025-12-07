e38324f7994a280cd57cc68c7dd84eb291e9d5d4
nurulabsarina <nurulabsarina.a@gmail.com>
2025-12-07 08:54:57 +0800

    Update viewproducts.php

--- snackshop/viewproducts.php
+++ snackshop/viewproducts.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
-    header("Location: login.php");
+    header("Location: index.php");
     exit;
 }
 require 'conn/db.php';
